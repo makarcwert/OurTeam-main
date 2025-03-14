a423b0539d4f177cb586b9e1503d6fd1d61b63b2
makarcwert <skrypkinmakar@gmail.com>
2025-03-15 00:22:03 +0300

    0.2.12.1

--- OurTeam-main/script.js
+++ OurTeam-main/script.js
@@ -1,27 +1,38 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвратить стандартное поведение формы
+window.onload = function() {
+    const loggedIn = localStorage.getItem('loggedIn');
+    const loginButton = document.getElementById('loginButton');
 
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    if (loggedIn) {
+        loginButton.innerText = 'Аккаунт'; // Change button text if logged in
+    } else {
+        loginButton.innerText = 'Войти'; // Default button text
+    }
 
-    fetch('register.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            'username': username,
-            'email': email,
-            'password': password
+    document.getElementById('registrationForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        fetch('register.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                'username': username,
+                'email': email,
+                'password': password
+            })
         })
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('message').innerText = data;
-    })
-    .catch((error) => {
-        console.error('Ошибка:', error);
-        document.getElementById('message').innerText = 'Ошибка при регистрации.';
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('message').innerText = data;
+        })
+        .catch((error) => {
+            console.error('Ошибка:', error);
+            document.getElementById('message').innerText = 'Ошибка при регистрации.';
+        });
     });
-});
+};
